1c5127d9d373d9eb0bda41a918da60807643b06e
Brendan Smull <brendanwsmull@gmail.com>
2025-02-27 16:14:12 -0500

    added logout button on pf screen as well as option for org/group accounts to update privacy settings

--- Event_Organizer/src/pages/ProfileScreen.jsx
+++ Event_Organizer/src/pages/ProfileScreen.jsx
@@ -1,15 +1,53 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './ProfileScreen.css'
 import { ProfileContext } from '../contexts/profileContext';
 import { CreateSubAccountScreen } from '../components/CreateSubComp'
 
 export function ProfileScreen() {
   const { profile, setProfile } = useContext(ProfileContext);
-  const [preferences, setPreferences] = useState('');
-  const [invite, setInvite] = useState('')
-  
+  const [ preferences, setPreferences ] = useState('');
+  const [ invite, setInvite ] = useState('');
+  const [ isPrivate, setIsPrivate ] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsPrivate(profile.isPrivate ? "Private" : "Public");
+  }, [])
+
+  const handleLogout = () => {
+    navigate('/');
+  }
+
+  const handlePrivate = async () => {
+    profile.isPrivate = !profile.isPrivate
+
+    const data = {
+        UUID: profile.uuid,
+        isPrivate: profile.isPrivate
+    };
+
+    const response = await fetch("http://localhost:5000/setPrivate", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (response.status === 200) {
+        alert("Change successful!");
+    } else {
+        alert("Something went wrong");
+    }
+    setIsPrivate(profile.isPrivate ? "Private" : "Public");
+  };
+
   return (
     <div>
+      <button className="logout-button" onClick={handleLogout}>
+        Log Out
+      </button>
       <h2>{profile.username}'s Profile</h2>
       {profile.accountType === 3 && (
         <div>
@@ -27,6 +65,7 @@ export function ProfileScreen() {
             placeholder="Enter User to invite to your group/organization"
           />
           <button>Invite User</button>
+          <p>This account is {isPrivate} <button onClick={handlePrivate}>toggle</button></p>
         </div>
       }
       {profile.accountType === 1 &&
